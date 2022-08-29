9c52947dcac2510afadd6345314b7e03c685a6a6
Amund Lindberg <amund.lindberg@knowit.no>
2022-08-29 11:19:09 +0200

    initial

---
+++ FruktApp/CountFruits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FruitApp
+{
+    internal class CountFruits
+    {
+        private static async Task Main(string[] args)
+        {
+            // Ctrl+F5 to run
+            var lines = await File.ReadAllLinesAsync("Fruits.txt");
+            foreach (var line in lines)
+            {
+                //Print
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
--- README.txt
+++ README.txt
@@ -1,4 +1,2 @@
-# fruktapp
-
 Oppgaven går ut på å telle antall forekomster av frukt som kan forekomme som anagrammer.
 Regnestykket står fruktfest.png
